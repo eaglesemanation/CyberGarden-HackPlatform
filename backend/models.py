509cc86cfddcb6291dff9e3a26fcbc649bc2453e
Vladimir Romashchenko <eaglesemanation@gmail.com>
2021-05-28 18:01:16 +0300

    feat(backend): Models for hackathon
    Added few models describing hackathon

--- backend/models.py
+++ backend/models.py
@@ -1,19 +1,51 @@
+from enum import Enum
+
 from tortoise import Model, Tortoise, fields
+
+
+class UserType(str, Enum):
+    participant = "participant"
+    capitan = "capitan"
+    organizer = "organizer"
+    admin = "admin"
 
 
 class User(Model):
     id = fields.IntField(pk=True)
     email = fields.CharField(max_length=128, unique=True)
     hashed_password = fields.CharField(max_length=512)
-
     fio = fields.CharField(null=True, max_length=128)
-    is_admin = fields.BooleanField(default=False)
+    type = fields.CharEnumField(UserType, default=UserType.participant)
 
     def __repr__(self):
         return str(self.email)
 
     class PydanticMeta:
-        exclude = ["hashed_password", "is_admin"]
+        exclude = ["hashed_password"]
+
+
+class Team(Model):
+    id = fields.IntField(pk=True)
+    name = fields.CharField(max_length=128)
+    teammates: fields.ManyToManyRelation[User] = fields.ManyToManyField(
+        "models.User", related_name="team"
+    )
+
+    def __repr__(self):
+        return str(self.name)
+
+
+class Hackathon(Model):
+    id = fields.IntField(pk=True)
+    name = fields.CharField(max_length=128)
+    description = fields.TextField()
+    image_url = fields.CharField(max_length=128)
+    teams: fields.ManyToManyRelation[Team] = fields.ManyToManyField(
+        "models.Team", related_name="hackathons"
+    )
+
+    def __repr__(self):
+        return str(self.name)
 
 
 Tortoise.init_models(["models"], "models")
