ffb0c1b958f13de06f3335e466da2b024ce4c912
Dmitriy Din <dmitriy1d01@gmail.com>
2021-05-28 19:01:46 +0300

    Prod works

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,9 +1,15 @@
+import adapter from '@sveltejs/adapter-node';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
+  kit: {
+    // hydrate the <div id="svelte"> element in src/app.html
+    // target: '#svelte'
+    adapter: adapter({
+      // default options are shown
+      out: 'build'
+    })
+  }
 };
 
 export default config;
