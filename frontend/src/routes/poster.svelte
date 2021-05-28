2416fba7bf2d47382f07aeabad21134a5df56cd8
kuvshinovmark <kuvshinovmark2001@gmail.com>
2021-05-28 16:52:40 +0300

    component for post
    компонент для афиши

--- frontend/src/routes/poster.svelte
+++ frontend/src/routes/poster.svelte
@@ -1,16 +1,23 @@
 <script>
 	import Post from '$lib/Post/index.svelte';
-	const forExample = {
-		name: "Cyber Garden",
-		date: "28.05.2021",
-		about: "супер пупер дупер хак"
-	};
+	const postsMass = [
+		{
+			name: "Cyber Garden",
+			date: "28.05.2021",
+			about: "супер пупер дупер хак"
+		},
+		{
+			name: "hack moon",
+			date: "33.06.2022",
+			about: "супер пупер дупер хак номер 2"
+		}
+	];//для примера, заполнять будем после
 </script>
 
 <svelte:head>
 	<title>poster</title>
 </svelte:head>
-
-
-<h1>poster</h1>
-<Post {...forExample}/>
+{#each postsMass as post}
+	<Post {...post}/>
+{/each}
+<!-- <Post {...forExample[1]}/> -->
