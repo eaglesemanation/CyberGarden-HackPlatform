bd415c04643c64f6c40889ee2acad05588cd23ad
kuvshinovmark <kuvshinovmark2001@gmail.com>
2021-05-28 18:20:25 +0300

    authAndReg

--- frontend/src/routes/authorization.svelte
+++ frontend/src/routes/authorization.svelte
@@ -1,14 +1,34 @@
 <script>
 	let mail = "";
 	let password = "";
+    function validate(){
 
+    }
 	function submit(){
-		console.log("start");
+        if(!validate()) return;
+		console.log("authorization");
 	}
 </script>
 
 <h1>authorization</h1>
-<input bind:value={mail} type="email">
-<input bind:value={password} type="password">
-<button on:click={submit}>Вход</button>
-<button><a sveltekit:prefetch href="/registration">Регистрация</a></button>
+<div class="authBox">
+    <input bind:value={mail} placeholder="Mail" type="email">
+    <input bind:value={password} placeholder="Пароль" type="password">
+    <div class="buttonsBox">
+        <button on:click={submit}>Вход</button>
+        <button><a sveltekit:prefetch href="/registration">Регистрация</a></button>
+    </div>
+</div>
+
+<style>
+	.authBox{
+		display: flex;
+		flex-direction: column;
+	}
+	.authBox input{
+		width: 200px;
+	}
+    .authBox button{
+		width: 100px;
+	}
+</style>
--- frontend/src/routes/registration.svelte
+++ frontend/src/routes/registration.svelte
@@ -1,13 +1,27 @@
 <script>
 	let mail = "";
-	let password = "";
+	let password1 = "";
+	let password2 = "";
 
 	function submit(){
-		console.log("start");
+		console.log("registration");
 	}
 </script>
 
 <h1>registration</h1>
-<input bind:value={mail} type="email">
-<input bind:value={password} type="password">
-<button on:click={submit}>Зарегистрировать</button>
+<div class="regBox">
+	<input bind:value={mail} placeholder="Mail" type="email">
+	<input bind:value={password1} placeholder="Пароль" type="password">
+	<input bind:value={password2} placeholder="Подтверждение пароля" type="password">
+	<button on:click={submit}>Зарегистрировать</button>
+</div>
+
+<style>
+	.regBox{
+		display: flex;
+		flex-direction: column;
+	}
+	.regBox *{
+		width: 200px;
+	}
+</style>
