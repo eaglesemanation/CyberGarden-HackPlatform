e201683c8b2110a878ba498dd4cbb9522bd228ff
kuvshinovmark <kuvshinovmark2001@gmail.com>
2021-05-29 16:50:54 +0300

    main

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -7,4 +7,55 @@
 	<title>Main</title>
 </svelte:head>
 
-<h1>main</h1>
+<div class="main">
+	<img src="./static/forMain.svg" />
+	<div class="text">
+		<h1 class="black">Garden<span class="green">Masters</span></h1>
+		<h3>Платформа для проведения мероприятий с упором на социальное взаимодействие и умную работу с данными</h3>
+	</div>
+</div>
+
+
+<style>
+	.main{
+		display: flex;
+		justify-content: space-around;
+		margin-top: 100px;
+	}
+	.green{
+		color: #43DFA8;
+	}
+	.black{
+		color: #00160A;
+		font-family: 'Ubuntu';
+		text-decoration: none;
+		font-size: calc(24px + (30 - 24) * ((100vw - 300px) / (1440 - 300)));
+		font-weight: 1000;
+	}
+	.text{
+		margin-top: 100px;
+	}
+	h3{
+		font-style: normal;
+		font-weight: 500;
+		font-size: 24px;
+		line-height: 29px;
+		color: rgba(0, 22, 10, 0.7);
+		width: 40vw;
+	}
+
+	@media (max-width: 850px){
+		.main{
+			flex-direction: column;
+			align-items: flex-start;
+    	}
+		img{
+			margin-left: auto;
+			margin-right: auto;
+		}
+		.text{
+			margin-left: auto;
+			margin-right: auto;
+		}
+	}
+</style>
