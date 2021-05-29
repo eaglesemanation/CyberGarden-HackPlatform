bab2db28e8adfeee100e511121f7e305334e3a17
Anastasia Gisina <anastasia.gisina@inbox.ru>
2021-05-29 10:21:24 +0300

    ended with auth page

--- frontend/src/routes/authorization.svelte
+++ frontend/src/routes/authorization.svelte
@@ -28,28 +28,59 @@
 	}
 </script>
 
-<h1>token in storage (svelte) - {Token}</h1>
-<div class="authBox">
-    <input bind:value={mail} placeholder="E-mail" type="email">
-    <input bind:value={password} placeholder="Пароль" type="password">
-    <div class="buttonsBox">
-        <button on:click={submit} class="auth-button">Вход</button>
-        <a sveltekit:prefetch href="/registration" class="reg-button">Регистрация</a>
+<div class="main-block">
+    <img src="./static/space.svg" />
+    <div class="auth-wrapper">
+        <h1>Зайти в <span class="green">пространство</span></h1>
+        <div class="authBox">
+            <input bind:value={mail} placeholder="E-mail" type="email">
+            <input bind:value={password} placeholder="Пароль" type="password">
+            <div class="buttonsBox">
+                <button on:click={submit} class="auth-button">Вход</button>
+                <a sveltekit:prefetch href="/registration" class="reg-button">Регистрация</a>
+            </div>
+        </div>
     </div>
 </div>
 
 <style>
     @import url('https://fonts.googleapis.com/css2?family=Red+Hat+Display:wght@700&family=Ubuntu:wght@400;500;700&display=swap');
 
+    .main-block {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        place-items: center;
+        height: 80vh;
+    }
+
+    img {
+        max-width: 90%;
+        height: auto;
+    }
+
+    .auth-wrapper {
+        margin: 1em;
+    }
+
 	.authBox{
 		display: flex;
 		flex-direction: column;
 	}
 
+    h1 {
+        font-family: 'Ubuntu';
+        font-size: calc(18px + (26 - 18) * ((100vw - 300px) / (1440 - 300)));
+		font-weight: 700;
+    }
+
+    .green {
+		color: #43DFA8;
+	}
+
 	input {
 		max-width: 400px;
         height: 48px;
-        margin: 1em;
         margin-bottom: 0.5em;
         border-radius: 8px;
         padding-left: 2em;
@@ -62,10 +93,6 @@
         outline: none;
         border: 1px solid #43DFA8;
         box-shadow: 0 0 10px rgba(67, 223, 168, 0.5);
-    }
-
-    .buttonsBox {
-        margin: 1em;
     }
 
     .auth-button {
@@ -88,6 +115,12 @@
 		border-radius: 30px;
 		padding: 0.75em 2em;
 		font-size: calc(14px + (18 - 14) * ((100vw - 300px) / (1440 - 300)));
+    }
+
+    @media (max-width: 1000px){ 
+        .main-block {
+            flex-direction: column;
+        }
     }
 
 </style>
