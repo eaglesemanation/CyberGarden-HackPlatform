f125df84c53055e70d678976cccd0c86786aaa4a
Anastasia Gisina <black_brain.vip@bk.ru>
2021-05-30 00:39:31 +0300

    conflicts

--- frontend/src/routes/events/create.svelte
+++ frontend/src/routes/events/create.svelte
@@ -10,6 +10,8 @@
 </script>
 
 <script>
+  import MainButton from '$lib/MainButton/index.svelte';
+
   let name = "";
   let description = "";
 
@@ -26,31 +28,75 @@
 
 
 <svelte:head>
-  <title>Создание хакатона</title>
+  <title>Создать мероприятие</title>
 </svelte:head>
 
-<h1>Новый хакатон</h1>
-<div class="inputBox">
-  <input bind:value={name} type="text" placeholder="name">
-  <input bind:value={date} type="text" placeholder="date">
-  <input bind:value={location_lon} type="text" placeholder="location">
-  <input bind:value={location_lat} type="text" placeholder="location">
-  <textarea class="description" bind:value={description} placeholder="description"></textarea>
-  <input bind:value={sponsor} type="text" placeholder="sponsor">
-  <input bind:value={organizer} type="text" placeholder="organizator">
+<div class="main-block">
+  <h1>event registration</h1>
+  <div class="input-wrapper">
+    <input bind:value={name} type="text" placeholder="name">
+    <label>Дата <span class="green">начала</span> хакатона</label>
+    <input bind:value={date} type="date" placeholder="Время начала хакатона">
+    <input bind:value={date} type="date" placeholder="Время окончания хакатона">
+    <textarea class="description" bind:value={description} placeholder="Описание"></textarea>
+    <input bind:value={sponsor} type="text" placeholder="Спонсоры">
+    <input bind:value={organizer} type="text" placeholder="Организаторы">
+  </div>
+  <MainButton on:click={create} btntext="Создать"></MainButton>
 </div>
-<button on:click={create}>create</button>
+
 <style>
-  .inputBox {
+
+  .main-block {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    place-items: center;
+    height: 100%;
+    margin: 1em;
+  }
+
+  .input-wrapper {
     display: flex;
     flex-direction: column;
   }
 
-  .inputBox * {
-    width: 300px;
+  input {
+    max-width: 600px;
+    height: 48px;
+    margin-bottom: 0.5em;
+    border-radius: 8px;
+    padding-left: 2em;
+    padding-right: 2em;
+    border: 1px solid #E1E3E6;
+    font-family: 'Ubuntu';
+    font-size: calc(14px + (18 - 14) * ((100vw - 300px) / (1440 - 300)));
   }
 
-  .inputBox .information {
+  textarea {
+    max-width: 600px;
     height: 200px;
+    margin-bottom: 0.5em;
+    border-radius: 8px;
+    padding-right: 2em;
+    padding-top: 1em;
+    padding-left: 2em;
+    border: 1px solid #E1E3E6;
+    font-family: 'Ubuntu';
+    font-size: calc(14px + (18 - 14) * ((100vw - 300px) / (1440 - 300)));
   }
+
+  textarea:focus {
+    outline: none;
+    border: 1px solid #43DFA8;
+    box-shadow: 0 0 10px rgba(67, 223, 168, 0.5);
+  }
+
+  input:focus {
+    outline: none;
+    border: 1px solid #43DFA8;
+    box-shadow: 0 0 10px rgba(67, 223, 168, 0.5);
+  }
+
 </style>
