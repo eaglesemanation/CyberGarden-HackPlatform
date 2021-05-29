ddf749d2c5d2504aaac94fe3c41bac279fbe930d
kuvshinovmark <kuvshinovmark2001@gmail.com>
2021-05-29 13:56:37 +0300

    account

--- frontend/src/routes/account.svelte
+++ frontend/src/routes/account.svelte
@@ -2,6 +2,7 @@
 	import { onMount } from "svelte";
 	let Token;
 	let Status;
+	let accountInfo = {mail:"vadim@zhopa.com", fio:"Vadim Kandratov", avatar:"https://images.app.goo.gl/XfutCMxgtQ6DV6BH6", status:"capitan"};
     onMount(() => {
         Token = localStorage.getItem("token");
 		Status = localStorage.getItem("status");
@@ -10,10 +11,26 @@
 		localStorage.clear();
 		window.location.replace("/");
 	}
+	function funcDelete(){
+		if(confirm("exactly?")){
+			alert("Deleting...")
+		}
+	}
 </script>
 <svelte:head>
 	<title>account</title>
 </svelte:head>
 
+<img src={accountInfo.avatar} alt="error">
+<h2>FIO: {accountInfo.fio}</h2>
+<h2>Status: {accountInfo.status}</h2>
 <h1>Token = {Token}, Status = {Status}</h1>
 <button on:click={funcOut}>OUT</button>
+<button on:click={funcDelete}>DELETE ACCOUNT</button>
+
+<style>
+	img{
+		width: 300px;
+		height: 300px;
+	}
+</style>
