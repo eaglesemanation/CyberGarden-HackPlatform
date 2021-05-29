0bd0c494c60ce82fe53b82beda1b62bdefd0f157
Anastasia Gisina <black_brain.vip@bk.ru>
2021-05-29 23:59:20 +0300

    add hover on main button

--- frontend/src/lib/MainButton/index.svelte
+++ frontend/src/lib/MainButton/index.svelte
@@ -1,20 +1,28 @@
 <script>
-    export let btntext;
+	export let btntext;
 </script>
 
-<button on:click class="main-button">{btntext}</button>
+<button on:click>{btntext}</button>
 
 <style>
 
-    .main-button {
-        background-color: #00160A;
-        color: #fff;
-        font-family: 'Ubuntu';
-        font-weight: 500;
-        border-color: #00160A;
-        border-radius: 30px;
-        padding: 0.75em 2em;
-        font-size: calc(14px + (18 - 14) * ((100vw - 300px) / (1440 - 300)));
-    }
+	button {
+		background-color: #00160A;
+		color: #fff;
+		font-family: 'Ubuntu';
+		font-weight: 500;
+		border: none;
+		border-radius: 30px;
+		padding: 0.75em 2em;
+		font-size: calc(14px + (18 - 14) * ((100vw - 300px) / (1440 - 300)));
+	}
+
+	button:hover {
+		background-color:#43DFA8;
+	}
+
+	button:focus {
+		outline: none;
+	}
 
 </style>
