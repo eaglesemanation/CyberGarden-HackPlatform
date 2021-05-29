0363fa8af773b487ec4c9dbdeedbdcf6874050dc
kuvshinovmark <kuvshinovmark2001@gmail.com>
2021-05-29 14:05:12 +0300

    Posters
    тупа дезигн

--- frontend/src/routes/account.svelte
+++ frontend/src/routes/account.svelte
@@ -2,7 +2,7 @@
 	import { onMount } from "svelte";
 	let Token;
 	let Status;
-	let accountInfo = {mail:"vadim@zhopa.com", fio:"Vadim Kandratov", avatar:"https://images.app.goo.gl/XfutCMxgtQ6DV6BH6", status:"capitan"};
+	let accountInfo = {mail:"vadim@zhopa.com", fio:"Vadim Kandratov", avatar:"http://sun9-39.userapi.com/s/v1/ig2/8aqATIMGN_0ucbrpPT2w9-Od9s4_R-28vuF1rs263b_AnT8lBidXi9Tp1qazfob7TONkocJPt4t4cK1Z6ZOpdx3e.jpg?size=200x0&quality=96&crop=35,35,1002,1009&ava=1", status:"capitan"};
     onMount(() => {
         Token = localStorage.getItem("token");
 		Status = localStorage.getItem("status");
