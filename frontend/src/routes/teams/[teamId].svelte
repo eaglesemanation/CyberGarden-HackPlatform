8d53e2127fee7a032998d273a8315ced1bbceb76
kuvshinovmark <kuvshinovmark2001@gmail.com>
2021-05-30 12:22:58 +0300

    all

--- frontend/src/routes/teams/[teamId].svelte
+++ frontend/src/routes/teams/[teamId].svelte
@@ -23,20 +23,32 @@
     {#if detail}
       <img src="https://static.tildacdn.com/tild3935-3365-4465-b965-396661666130/img.svg" alt="404 not found">
     {:else}
+      <h1 class="name">{name}</h1>
+      <h2>Ссылка для пришлашения: </h2>
       <h1>{invite_link}</h1>
     {/if}
-    <!--  <h1>{JSON.stringify(data)}</h1>-->
-    <!--  <h1>{name}</h1>-->
-    <!--  <h1>{participants}</h1>-->
-    <!--  <h1>{invite_link}</h1>-->
+     <!-- <h1>{JSON.stringify(data)}</h1> -->
   {/await}
 </div>
 <style>
+  .name{
+      margin-top: 100px;
+        font-family: 'Ubuntu';
+        font-size: calc(34px + (26 - 18) * ((100vw - 300px) / (1440 - 300)));
+        color: #00160A;
+        margin-bottom: 0;
+        font-weight: 700;
+    }
+  h2{
+    margin-top: 5 0px;
+  }
+  h1{
+    margin-top: -10px;
+  }
   img {
     width: 80%;
     max-width: 600px;
   }
-
   .page {
     width: 100%;
     height: 100%;
--- frontend/src/routes/teams/enter-[token].svelte
+++ frontend/src/routes/teams/enter-[token].svelte
@@ -15,13 +15,80 @@
 
 <script>
   export let promise;
-
+  const mass = {hack: "Cyber Garden", capitan: "Дмитрий Дин"}
 </script>
 
 <div>
   {#await $promise}
-  {:then d}
-    <p>{JSON.stringify(d)}</p>
+  {:then telo}
+    <!-- <p>{JSON.stringify(telo)}</p> -->
+    <div class="mainBox">
+      <h1>{telo.name}</h1>
+      <div class="bot">
+        <div class="info">
+          <h2>Capitan: {mass.capitan}</h2>
+          <h2>Hackathon: {mass.hack}</h2>
+        </div>
+        <div class="a-box">
+          <a on:click={funcSearch(posts)}>
+            <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" clip-rule="evenodd"
+                    d="M2 8C2 4.691 4.691 2 8 2C11.309 2 14 4.691 14 8C14 11.309 11.309 14 8 14C4.691 14 2 11.309 2 8ZM17.707 16.293L14.312 12.897C15.365 11.543 16 9.846 16 8C16 3.589 12.411 0 8 0C3.589 0 0 3.589 0 8C0 12.411 3.589 16 8 16C9.846 16 11.543 15.365 12.897 14.312L16.293 17.707C16.488 17.902 16.744 18 17 18C17.256 18 17.512 17.902 17.707 17.707C18.098 17.316 18.098 16.684 17.707 16.293Z"
+                    fill="#757D8A"/>
+            </svg>
+          </a>
+        </div>
+      </div>
+      
+    </div>
+    
   {/await}
-  <h1>Hello</h1>
+  <h2 style="text-align:center">Поздравляем, ваш запрос отправлен на рассмотрение командира команды!</h2>
 </div>
+
+<style>
+  .bot{
+    display: flex;
+    justify-content: space-between;
+  }
+  h1{
+        font-family: 'Ubuntu';
+        font-size: calc(34px + (26 - 18) * ((100vw - 300px) / (1440 - 300)));
+        color: #00160A;
+        margin-bottom: 20px;
+        font-weight: 700;
+  }
+  h2{
+    margin-top: 0px;
+    margin-bottom: 0px;
+  }
+  .mainBox{
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    margin-top: 30px;
+    background-color: #ffffff;
+    border: 3px solid #E1E3E6;
+    box-sizing: border-box;
+    border-radius: 30px;
+    width: 600px;
+    margin-left: auto;
+    margin-right: auto;
+    padding: 2%;
+  }
+  .a-box {
+    height: 52px;
+    background-color: whitesmoke;
+    border: 1px solid #E1E3E6;
+    width: 40px;
+    /* margin-left: -5px; */
+  }
+  .a-box:hover {
+    background-color: rgb(228, 225, 225);
+    transition: 0.7s;
+  }
+  .a-box:active {
+    background-color: rgb(199, 199, 199);
+    transition: 0.1s;
+  }
+</style>
