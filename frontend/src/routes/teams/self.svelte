b39e1d1bacbe489e26e64761e4ad0c88db43a83c
Anastasia Gisina <black_brain.vip@bk.ru>
2021-05-30 11:04:54 +0300

    some beatiful

--- frontend/src/routes/teams/self.svelte
+++ frontend/src/routes/teams/self.svelte
@@ -16,22 +16,23 @@
 </script>
 
 <svelte:head>
-  <title>teaming</title>
+  <title>Мои команды</title>
 </svelte:head>
 <div class="main">
-
+  <h1>Мои <span class="green">команды</span></h1>
   <div class="buttons">
     <div>
-      <input placeholder="Токен команды" bind:value={teamToken}>
+      <input placeholder="Добавить команду по токену" bind:value={teamToken}>
     </div>
     <div class="a-box">
-     <a sveltekit:prefetch href="/teams/enter-{teamToken}"><svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <line y1="14" x2="30" y2="14" stroke="#757D8A" stroke-width="2"/>
-      <line x1="15" y1="30" x2="15" stroke="#757D8A" stroke-width="2"/>
-      </svg>
+     <a class="box" sveltekit:prefetch href="/teams/enter-{teamToken}">
+        <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <line y1="14" x2="30" y2="14" stroke="#757D8A" stroke-width="2"/>
+        <line x1="15" y1="30" x2="15" stroke="#757D8A" stroke-width="2"/>
+        </svg>
       </a>
     </div>
-    <a class="main-button" sveltekit:prefetch href="/teams/create">Создать команду</a>
+    <a class="main-button" sveltekit:prefetch href="/teams/create">Создать новую команду</a>
   </div>
   
 
@@ -46,6 +47,27 @@
 
 
 <style>
+
+  h1 {
+    font-family: 'Ubuntu';
+    font-weight: 700;
+    font-size: calc(18px + (26 - 18) * ((100vw - 300px) / (1440 - 300)));
+  }
+
+  .green {
+    color: #43DFA8;
+  }
+
+  .main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    place-items: center;
+    height: 80vh;
+    margin: 1em;
+  }
+
   .buttons{
     display: flex;
   }
@@ -55,14 +77,14 @@
   }
   input{
     width: 300px;
-    height: 50px;
+    height: 3em;
     border-radius: 8px 0px 0px 8px;
     border: 1px solid #E1E3E6;
     font-family: 'Ubuntu';
     font-size: calc(14px + (18 - 14) * ((100vw - 300px) / (1440 - 300)));
   }
   .a-box {
-    height: 52px;
+    height: 3.472em;
     background-color: whitesmoke;
     border: 1px solid #E1E3E6;
     width: 60px;
