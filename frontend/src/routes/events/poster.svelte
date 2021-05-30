00e57772046a64c77cbf051d9c0a43231a881add
kuvshinovmark <kuvshinovmark2001@gmail.com>
2021-05-30 07:19:49 +0300

    fixed search
    не робит enter

--- frontend/src/routes/events/poster.svelte
+++ frontend/src/routes/events/poster.svelte
@@ -38,14 +38,15 @@
   let postsMass = posts;
 
   //его строем после поиска
-  function funcSearch() {
+  function funcSearch(massF) {
+    // console.log(massF);
     postsMass = [];
     if (selectedCity === "All") selectedCity = "";
     if (searchLine === "" && selectedCity === "") {
-      postsMass = posts.concat();
+      postsMass = massF.concat();
       return;
     } else if (selectedCity !== "") {
-      posts.forEach(element => {
+      massF.forEach(element => {
         let hackCity = element.location.toLowerCase();
         if (hackCity.indexOf(selectedCity.toLowerCase()) !== -1) {
           postsMass = postsMass.concat(element);
@@ -53,7 +54,7 @@
       });
       if (searchLine !== "") {
         postsMass = [].concat();
-        posts.forEach(element => {
+        massF.forEach(element => {
           let hackCity = element.location.toLowerCase();
           let hackName = element.name.toLowerCase();
           if ((hackName.indexOf(searchLine.toLowerCase()) !== -1) && (hackCity.indexOf(selectedCity.toLowerCase()) !== -1)) {
@@ -62,13 +63,16 @@
         });
       }
     } else {
-      posts.forEach(element => {
+      massF.forEach(element => {
         let hackName = element.name.toLowerCase();
         if (hackName.indexOf(searchLine.toLowerCase()) !== -1) {
           postsMass = postsMass.concat(element);
         }
       });
     }
+    // console.log(postsMass);
+    // posts = [];
+    return;
   }
 
   function handleKeydown(event) {
@@ -83,13 +87,16 @@
 </svelte:head>
 
 
-<svelte:window on:keydown={handleKeydown}/>
+<!-- <svelte:window on:keydown={handleKeydown}/> -->
+{#await $promise}
+  {:then posts}
+<h1 style="font-size:0px">{funcSearch(posts)}</h1>
 <div class="main">
 
   <div class="search">
     <input bind:value={searchLine} type="text">
     <div class="a-box">
-      <a on:click={funcSearch}>
+      <a on:click={funcSearch(posts)}>
         <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path fill-rule="evenodd" clip-rule="evenodd"
                 d="M2 8C2 4.691 4.691 2 8 2C11.309 2 14 4.691 14 8C14 11.309 11.309 14 8 14C4.691 14 2 11.309 2 8ZM17.707 16.293L14.312 12.897C15.365 11.543 16 9.846 16 8C16 3.589 12.411 0 8 0C3.589 0 0 3.589 0 8C0 12.411 3.589 16 8 16C9.846 16 11.543 15.365 12.897 14.312L16.293 17.707C16.488 17.902 16.744 18 17 18C17.256 18 17.512 17.902 17.707 17.707C18.098 17.316 18.098 16.684 17.707 16.293Z"
@@ -110,14 +117,13 @@
 
 
 <div class="events-block">
-  {#await $promise}
-  {:then posts}
-    {#each posts as post}
+  
+    {#each postsMass as post}
       <Post {...post}/>
     {/each}
-  {/await}
+  
 </div>
-
+{/await}
 <style>
   .main {
     display: flex;
